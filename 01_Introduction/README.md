e98573afeb5fe099c94f68a143baef47a7a8ec04
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-03-01 13:20:33 +0500

    Introduction

--- 01_Introduction/README.md
+++ 01_Introduction/README.md
@@ -1,5 +1,9 @@
 
-
+## Components
 - index.html - Structure
 - style.css - Style
 - index.js - Actions
+
+![alt text](image.png)
+
+![alt text](image-1.png)
---
+++ 01_Introduction/index.js
@@ -0,0 +1,14 @@
+// Outputs
+console.log(`Hello World`);
+console.log(`I am learning JavaScript`);
+
+// Alerts
+window.alert('This is an alert message');
+window.alert('This is another alert message');
+
+/*
+Multiline Comment
+*/
+
+// Changing text content of an element
+document.getElementById("MyH2").textContent = "This is a new heading";
