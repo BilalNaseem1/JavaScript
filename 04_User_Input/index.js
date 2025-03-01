cc8b44b1cee50efba5444d69728ff3f0e9e95546
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-03-01 15:07:30 +0500

    user input incomplete

---
+++ 04_User_Input/index.js
@@ -0,0 +1,19 @@
+// There are two ways to get user input
+// 1. Easy way - window prompt()
+// 2. Professional way - HTML Text Box
+
+
+// 1. Easy way - window prompt()
+let username = prompt('What is your name?');    
+let isStudent = prompt('Are you a student?');
+
+let name = window.prompt('What is your name?');
+
+console.log(username, isStudent);
+console.log(name);
+
+// 2. Professional way - HTML Text Box
+let usernameInput;
+document.getElementById("mySubmit").oneclick = function() {
+    
+}
