034c2fdeb459e7639acc5c7384b793ee6f8dc7d8
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-03-07 22:08:02 +0500

    Need to start arrays

---
+++ 24_TempConversionProgram/index.js
@@ -0,0 +1,25 @@
+
+// function celsiusToFahrenheit(celsius) {
+//     return (celsius * 9 / 5) + 32;
+// }
+
+// function fahrenheitToCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5 / 9;
+// }
+
+// const celsiusInput = document.getElementById("celsius");
+// const fahrenheitInput = document.getElementById("fahrenheit");
+// const celsiusToButton = document.getElementById("celsiusToButton");
+// const fahrenheitToButton = document.getElementById("fahrenheitToButton");
+
+// celsiusToButton.addEventListener("click", function() {
+//     const celsius = parseFloat(celsiusInput.value);
+//     const fahrenheit = celsiusToFahrenheit(celsius);
+//     fahrenheitInput.value = fahrenheit.toFixed(2);
+// });
+
+// fahrenheitToButton.addEventListener("click", function() {
+//     const fahrenheit = parseFloat(fahrenheitInput.value);    
+//     const celsius = fahrenheitToCelsius(fahrenheit);
+//     celsiusInput.value = celsius.toFixed(2);
+// });
