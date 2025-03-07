bb9a2e1c839540e20b110db07f9433101016d656
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-03-07 22:40:01 +0500

    arrays done

---
+++ 25_Arrays/index.js
@@ -0,0 +1,11 @@
+// arrays
+
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+let myArray2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(myArray2);
+
+// accessing elements in an array
+console.log(myArray[0]);
+console.log(myArray2[1]);
